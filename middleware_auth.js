ebc93f5c6fdcd89c23adbc58024c5f6c070ed107
Jk87ppihjk <tutano172@gmail.com>
2025-11-29 09:48:41 -0300

    Create middleware_auth.js

---
+++ middleware_auth.js
@@ -0,0 +1,26 @@
+const jwt = require('jsonwebtoken');
+
+const verifyToken = (req, res, next) => {
+    const token = req.headers['authorization'];
+    if (!token) return res.status(403).json({ message: "Token necessário" });
+
+    try {
+        const decoded = jwt.verify(token.split(" ")[1], process.env.JWT_SECRET);
+        req.user = decoded;
+        next();
+    } catch (err) {
+        return res.status(401).json({ message: "Token inválido" });
+    }
+};
+
+const verifyAdmin = (req, res, next) => {
+    verifyToken(req, res, () => {
+        if (req.user.isAdmin) {
+            next();
+        } else {
+            res.status(403).json({ message: "Acesso negado. Apenas Admins." });
+        }
+    });
+};
+
+module.exports = { verifyToken, verifyAdmin };
